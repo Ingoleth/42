4a8951d710e5898042aebf7b7f57d08d80151067
aiglesia <Ingoleth@github.com>
2020-11-12 20:44:07 +0100

    ray_handler norminette + cleanup

--- cub3d/ray_tracing/ray_handler.c
+++ cub3d/ray_tracing/ray_handler.c
@@ -6,7 +6,7 @@
 /*   By: aiglesia <aiglesia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 14:26:03 by aiglesia          #+#    #+#             */
-/*   Updated: 2020/11/12 16:43:58 by aiglesia         ###   ########.fr       */
+/*   Updated: 2020/11/12 20:43:31 by aiglesia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,12 @@
 ** If the opposing coordinate increases, do 1 - offset!
 */
 
-int calculate_collision_0(float p_x, float p_y, s_ray_tracing *ray_trc, char **map)
+int	calculate_collision_0(float p_x, float p_y, s_ray_tracing *ray_trc,
+char **map)
 {
 	int x;
 	int y;
-	
+
 	x = (int)p_x;
 	y = (int)p_y;
 	ray_trc->x_collision = p_x + (p_y - y) * ray_trc->tan_x;
@@ -29,23 +30,24 @@ int calculate_collision_0(float p_x, float p_y, s_ray_tracing *ray_trc, char **m
 	{
 		while ((ray_trc->x_collision - x) <= (1 + y - ray_trc->y_collision))
 		{
-			if(map[(y = y - 1)][x] == '1')
-				return((ray_trc->cardinal_collision = SOUTH) &&
+			if (map[--y][x] == '1')
+				return ((ray_trc->cardinal_collision = SOUTH) &&
 				(ray_trc->y_collision = y + 1));
-			ray_trc->x_collision += ray_trc->tan_x;
+				ray_trc->x_collision += ray_trc->tan_x;
 		}
 		while ((ray_trc->x_collision - x) > (1 + y - ray_trc->y_collision))
 		{
-			if(map[y][(x = x + 1)] == '1')
-				return((ray_trc->cardinal_collision = WEST) &&
+			if (map[y][(++x)] == '1')
+				return ((ray_trc->cardinal_collision = WEST) &&
 				(ray_trc->x_collision = x));
-			ray_trc->y_collision -= ray_trc->tan_y;
+				ray_trc->y_collision -= ray_trc->tan_y;
 		}
 	}
-	return(0);
+	return (0);
 }
 
-int calculate_collision_1(float p_x, float p_y, s_ray_tracing *ray_trc, char **map)
+int	calculate_collision_1(float p_x, float p_y, s_ray_tracing *ray_trc,
+char **map)
 {
 	int x;
 	int y;
@@ -58,26 +60,28 @@ int calculate_collision_1(float p_x, float p_y, s_ray_tracing *ray_trc, char **m
 	{
 		while ((1 + x - ray_trc->x_collision) <= (1 + y - ray_trc->y_collision))
 		{
-			if(map[(y = y - 1)][x] == '1')
-				return((ray_trc->cardinal_collision = SOUTH) &&
+			if (map[--y][x] == '1')
+				return ((ray_trc->cardinal_collision = SOUTH) &&
 				(ray_trc->y_collision = y + 1));
-			ray_trc->x_collision -= ray_trc->tan_x;
+				ray_trc->x_collision -= ray_trc->tan_x;
 		}
 		while ((1 + x - ray_trc->x_collision) > (1 + y - ray_trc->y_collision))
 		{
-			if(map[y][(x = x - 1)] == '1')
-				return((!(ray_trc->cardinal_collision = EAST)) && (ray_trc->x_collision = x + 1));
-			ray_trc->y_collision -= ray_trc->tan_y;
+			if (map[y][--x] == '1')
+				return ((!(ray_trc->cardinal_collision = EAST)) &&
+				(ray_trc->x_collision = x + 1));
+				ray_trc->y_collision -= ray_trc->tan_y;
 		}
 	}
-	return(0);
+	return (0);
 }
 
-int calculate_collision_2(float p_x, float p_y, s_ray_tracing *ray_trc, char **map)
+int	calculate_collision_2(float p_x, float p_y, s_ray_tracing *ray_trc,
+char **map)
 {
 	int x;
 	int y;
-	
+
 	x = (int)p_x;
 	y = (int)p_y;
 	ray_trc->x_collision = p_x - (1 - p_y + y) * ray_trc->tan_x;
@@ -86,23 +90,24 @@ int calculate_collision_2(float p_x, float p_y, s_ray_tracing *ray_trc, char **m
 	{
 		while ((1 + x - ray_trc->x_collision) <= (ray_trc->y_collision - y))
 		{
-			if(map[(y = y + 1)][x] == '1')
-				return((ray_trc->cardinal_collision = NORTH) &&
+			if (map[++y][x] == '1')
+				return ((ray_trc->cardinal_collision = NORTH) &&
 				(ray_trc->y_collision = y));
-			ray_trc->x_collision -= ray_trc->tan_x;
+				ray_trc->x_collision -= ray_trc->tan_x;
 		}
 		while ((1 + x - ray_trc->x_collision) > (ray_trc->y_collision - y))
 		{
-			if(map[y][(x = x - 1)] == '1')
-				return((!(ray_trc->cardinal_collision = EAST)) &&
+			if (map[y][--x] == '1')
+				return ((!(ray_trc->cardinal_collision = EAST)) &&
 				(ray_trc->x_collision = x + 1));
-			ray_trc->y_collision += ray_trc->tan_y;
+				ray_trc->y_collision += ray_trc->tan_y;
 		}
 	}
-	return(0);
+	return (0);
 }
 
-int calculate_collision_3(float p_x, float p_y, s_ray_tracing *ray_trc, char **map)
+int	calculate_collision_3(float p_x, float p_y, s_ray_tracing *ray_trc,
+char **map)
 {
 	int x;
 	int y;
@@ -115,18 +120,18 @@ int calculate_collision_3(float p_x, float p_y, s_ray_tracing *ray_trc, char **m
 	{
 		while ((ray_trc->x_collision - x) <= (ray_trc->y_collision - y))
 		{
-			if(map[(y = y + 1)][x] == '1')
-				return((ray_trc->cardinal_collision = NORTH) &&
+			if (map[++y][x] == '1')
+				return ((ray_trc->cardinal_collision = NORTH) &&
 				(ray_trc->y_collision = y));
-			ray_trc->x_collision += ray_trc->tan_x;
+				ray_trc->x_collision += ray_trc->tan_x;
 		}
 		while ((ray_trc->x_collision - x) > (ray_trc->y_collision - y))
 		{
-			if(map[y][(x = x + 1)] == '1')
-				return((ray_trc->cardinal_collision = WEST) &&
+			if (map[y][++x] == '1')
+				return ((ray_trc->cardinal_collision = WEST) &&
 				(ray_trc->x_collision = x));
-			ray_trc->y_collision += ray_trc->tan_y;
+				ray_trc->y_collision += ray_trc->tan_y;
 		}
 	}
-	return(0);
+	return (0);
 }
