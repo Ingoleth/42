f717c2703d90c5538f855282bdd01c98ea2d22f9
aiglesia <Ingoleth@github.com>
2020-12-19 11:54:11 +0100

    Let's test!

--- cub3d/srcs/file_reading/fill_render_struct.c
+++ cub3d/srcs/file_reading/fill_render_struct.c
@@ -6,7 +6,7 @@
 /*   By: aiglesia <aiglesia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/24 16:34:39 by user42            #+#    #+#             */
-/*   Updated: 2020/12/19 11:33:13 by aiglesia         ###   ########.fr       */
+/*   Updated: 2020/12/19 11:53:32 by aiglesia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,6 +101,7 @@ int		handle_textures(char *line, s_render_data *render_data, s_error *error)
 void	fill_render_struct(s_render_data *render_data, char *line,
 s_error *error, s_file_descriptor *file)
 {
+	char *aux;
 
 	if (*line == 'R')
 		fill_resolution(line + 1, render_data, error);
@@ -110,7 +111,8 @@ s_error *error, s_file_descriptor *file)
 		fill_colour(&render_data->c_rgb, skip_spaces(line + 1), error);
 	else if (line[0] == 'F')
 		fill_colour(&render_data->f_rgb, skip_spaces(line + 1), error);
-	else if (*line == '1' || *line == ' ')
+	else if ((*line == '1' || *line == ' ') &&
+	(aux = line) && !advance_numbers(&aux, error, false))
 	{
 		handle_map(render_data, error, line, file);
 		return ;
