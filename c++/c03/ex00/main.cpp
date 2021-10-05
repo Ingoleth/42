b1736d6e1727d04d24f3b0e05980c677eb769050
Ingoleth <aiglesia97@gmail.com>
2021-10-05 12:52:29 +0200

    c03/ex00 cleaned up and ready to go'!'

--- c++/c03/ex00/main.cpp
+++ c++/c03/ex00/main.cpp
@@ -6,14 +6,15 @@ int main()
 	ClapTrap b(a);
 
 	std::cout << a;
+	std::cout << "\nB output, to prove it has been properly initialized via copy:\n" << b;
 	b.setName("Fred");
 	b.setAttackDamage(15);
-	std::cout << b;
+	std::cout << "\nB output, to prove the changes:\n" << b;
 	b = a;
-	std::cout << b;
+	std::cout << "\nB output, to prove it has been properly copied:\n" << b;
+	b.setName("Fred"); //Name is changed again for attack test and destructor
 	a.attack("Pete");
 	a.takeDamage(5);
 	a.beRepaired(10);
-	b.setName("Fred");
 	return 0;
 }
