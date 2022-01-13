6706a16ec7fa8b46c5b67d9a89916f1634700ee3
aiglesia <Ingoleth@github.com>
2022-01-13 11:13:12 +0100

    Moved tree manipulation functions to the map class; Implemented comparison template argument usage

--- ft_containers/BinaryTree.hpp
+++ ft_containers/BinaryTree.hpp
@@ -29,7 +29,6 @@ namespace ft
 
 		BTNode(const T& _data, BTNode *parent) : data(_data)
 		{
-
 			top = parent;
 			left = 0;
 			right = 0;
@@ -91,6 +90,36 @@ namespace ft
 			return node;
 		}
 
+		BTNode *getNext(BTNode *currentNode)
+		{
+			if (currentNode->right)
+				return (findSmallestNode(currentNode->right));
+			while (true)
+			{
+				if (!currentNode)
+					break;
+				if (currentNode->top->left == currentNode)
+					return (currentNode->top);
+				currentNode = currentNode->top;
+			}
+			return (0);
+		}
+
+		BTNode *getPrevious(BTNode *currentNode)
+		{
+			if (currentNode->left)
+				return (findBiggestNode(currentNode->left));
+			while (true)
+			{
+				if (!currentNode)
+					break;
+				if (currentNode->top->right == currentNode)
+					return (currentNode->top);
+				currentNode = currentNode->top;
+			}
+			return (0);
+		}
+
 		void displayNodeInfo()
 		{
 			std::cout << "\n-------------------------------\n";
@@ -126,148 +155,5 @@ namespace ft
 		}
 	};
 
-		template <class T>
-		BTNode<T> *findInNode(T to_find, BTNode<T> *node)
-		{
-			if (!node)
-				return (NULL);
-			if (to_find == node->data)
-				return (node);
-			else if (to_find < node->data)
-				return (findInNode(to_find, node->left));
-			return (findInNode(to_find, node->right));
-		}
-
-		template <class T>
-		BTNode<T> *findInNode(T to_find, BTNode<T> *node, BTNode<T> **lastNode)
-		{
-			if (!node)
-				return (NULL);
-			if (to_find == node->data)
-				return (node);
-			*lastNode = node;
-			if (to_find < node->data)
-				return (findInNode(to_find, node->left, lastNode));
-			return (findInNode(to_find, node->right, lastNode));
-		}
-
-		template <typename T>
-		BTNode<T> *getNext(BTNode<T> *currentNode) // Check return value when biggest num?
-		{
-			if (currentNode->right)
-				return (findSmallestNode(currentNode->right));
-			T &auxdata = currentNode->data;
-			while (true)
-			{
-				currentNode = currentNode->top;
-				if (!currentNode)
-					break;
-				if (currentNode->data > auxdata)
-					return (currentNode);
-			}
-			return (0);
-		}
-
-		template <typename T>
-		BTNode<T> *getPrevious(BTNode<T> *currentNode) // Check return value when biggest num?
-		{
-			if (currentNode->left)
-				return (findBiggestNode(currentNode->left));
-			T &auxdata = currentNode->data;
-			while (true)
-			{
-				currentNode = currentNode->top;
-				if (!currentNode)
-					break;
-				if (currentNode->data < auxdata)
-					return (currentNode);
-			}
-			return (0);
-		}
-
-		template <class T>
-		pair<ft::BTNode<T> *, bool>addNode(const T& _data, BTNode<T> **root) //There's much to do here...
-		{
-			BTNode<T> *aux;
-
-			if (!*root)
-			{
-				*root = new BTNode<T>(_data);
-				return ft::make_pair(*root, true);
-			}
-			if (findInNode(_data, *root, &aux))
-				return ft::make_pair(aux, false);
-			return(ft::make_pair(new BTNode<T>(_data, aux), true));
-		}
-
-
-		template <class T>
-		void insert_node(BTNode<T> *node, BTNode<T> *parent)
-		{
-			if (!node || !parent)
-				return ;
-			findInNode(node->data, parent, &parent);
-			if (parent->data < node->data)
-				parent->right = node;
-			else
-				parent->left = node;
-			node->top = parent;
-		}
-
-		template <class T>
-		void balanceTree(BTNode<T> **Node)
-		{
-			if (!*Node)
-				return;
-
-			int imbalance = heigth((*Node)->left) - heigth((*Node)->right);
-			BTNode<T> *aux = NULL;
-
-			while (imbalance <= -2)
-			{
-				aux = *Node;
-				*Node = (*Node)->right;
-				(*Node)->top = aux->top;
-				aux->right = NULL;
-				insert_node(aux, *Node);
-				imbalance += 2;
-			}
-			while (imbalance >= 2)
-			{
-				aux = *Node;
-				*Node = (*Node)->left;
-				(*Node)->top = aux->top;
-				aux->left = NULL;
-				insert_node(aux, *Node);
-				imbalance -= 2;
-			}
-			balanceTree(&(*Node)->left);
-			balanceTree(&(*Node)->right);
-		}
-
-		template <class T>
-		void removeNode(const T& _data, BTNode<T> **root)
-		{
-			BTNode<T> *aux = NULL;
-
-			if (!*root || !(aux = findInNode(_data, *root)))
-				return ;
-			if (*root == aux)
-			{
-				*root = (*root)->right;
-				(*root)->top = NULL;
-				insert_node(aux->left, *root);
-			}
-			else
-			{
-				if (aux->top->data < aux->data)
-					aux->top->right = NULL;
-				else
-					aux->top->left = NULL;
-				insert_node(aux->left, aux->top);
-				insert_node(aux->right, aux->top);
-			}
-			delete aux;
-		}
 }
 #endif
--- ft_containers/Map.hpp
+++ ft_containers/Map.hpp
@@ -105,6 +105,7 @@ namespace ft
 		typedef typename allocator_type::const_pointer			const_pointer;
 		typedef typename allocator_type::reference				reference;
 		typedef typename allocator_type::const_reference		const_reference;
+		typedef BTNode<value_type>	*							binary_tree;
 
 		typedef ft::MapIterator<value_type>						iterator;
 		typedef ft::MapIterator<const value_type>				const_iterator;
@@ -113,11 +114,29 @@ namespace ft
 		typedef typename allocator_type::size_type				size_type;
 		//typedef typename _Rep_type::difference_type			difference_type;
 
+		class value_compare
+		{
+			friend class map;
+		protected:
+			key_compare comp;
+			value_compare (key_compare c): comp(c) {}
+		
+		public:
+			typedef bool result_type;
+			typedef value_type first_argument_type;
+			typedef value_type second_argument_type;
+			bool operator() (const value_type& x, const value_type& y) const
+			{
+				return (comp(x.first, y.first));
+			}
+		};
+
+
 	/*									 
 	** --------------------------------- CONSTRUCTOR ---------------------------------
 	*/
 
-		map() : tree(NULL), _mem() {}
+		map() : tree(NULL), _mem(), _compare(), _allocator() {}
 
 		~map()
 		{
@@ -233,7 +252,7 @@ namespace ft
 			erase(first);
 	}
 
-	void swap (ft::map< _Key, _Tp, _Compare, _Alloc>& x)
+	void swap (ft::map< _Key, _Tp, key_compare, _Alloc>& x)
 	{
 		swap(this->tree, x.tree);
 		swap(this->_mem, x._mem);
@@ -247,6 +266,11 @@ namespace ft
 	/*
 	** --------------------------------- OBSERVERS ---------------------------------
 	*/
+
+
+		key_compare key_comp() const { return _compare; }
+
+		value_compare value_comp() const { return value_compare(key_compare()); }
 
 	/*
 	** --------------------------------- OPERATIONS --------------------------------
@@ -293,9 +317,117 @@ namespace ft
 			return (_mem);
 		}
 
+	protected:
+		binary_tree			tree;
+		allocator_type		_mem;
+		key_compare			_compare;
+		allocator_type		_allocator;
+	
 	private:
-		BTNode<value_type> *tree;
-		allocator_type _mem;
+
+		binary_tree findInNode(value_type to_find, binary_tree node)
+		{
+			if (!node)
+				return (NULL);
+			if (to_find == node->data)
+				return (node);
+			if (_compare(to_find.first, (node->data).first))
+				return (findInNode(to_find, node->left));
+			return (findInNode(to_find, node->right));
+		}
+
+		binary_tree findInNode(value_type to_find, binary_tree node, binary_tree *lastNode)
+		{
+			if (!node)
+				return (NULL);
+			if (to_find == node->data)
+				return (node);
+			*lastNode = node;
+			if(_compare(to_find.first, (node->data).first))
+
+				return (findInNode(to_find, node->left, lastNode));
+			return (findInNode(to_find, node->right, lastNode));
+		}
+
+		pair<binary_tree, bool>addNode(const value_type& _data, binary_tree *root)
+		{
+			binary_tree aux;
+
+			if (!*root)
+			{
+				*root = new BTNode<value_type>(_data);
+				return ft::make_pair(*root, true);
+			}
+			if (findInNode(_data, *root, &aux))
+				return ft::make_pair(aux, false);
+			return(ft::make_pair(new BTNode<value_type>(_data, aux), true));
+		}
+
+		void insert_node(binary_tree node, binary_tree parent)
+		{
+			if (!node || !parent)
+				return ;
+			findInNode(node->data, parent, &parent);
+			if (_compare((parent->data).first, (node->data).first))
+				parent->right = node;
+			else
+				parent->left = node;
+			node->top = parent;
+		}
+
+		void balanceTree(binary_tree *Node)
+		{
+			if (!*Node)
+				return;
+
+			int imbalance = heigth((*Node)->left) - heigth((*Node)->right);
+			binary_tree aux = NULL;
+
+			while (imbalance <= -2)
+			{
+				aux = *Node;
+				*Node = (*Node)->right;
+				(*Node)->top = aux->top;
+				aux->right = NULL;
+				insert_node(aux, *Node);
+				imbalance += 2;
+			}
+			while (imbalance >= 2)
+			{
+				aux = *Node;
+				*Node = (*Node)->left;
+				(*Node)->top = aux->top;
+				aux->left = NULL;
+				insert_node(aux, *Node);
+				imbalance -= 2;
+			}
+			balanceTree(&(*Node)->left);
+			balanceTree(&(*Node)->right);
+		}
+
+		void removeNode(const value_type& _data, binary_tree *root)
+		{
+			binary_tree aux = NULL;
+
+			if (!*root || !(aux = findInNode(_data, *root)))
+				return ;
+			if (*root == aux)
+			{
+				*root = (*root)->right;
+				(*root)->top = NULL;
+				insert_node(aux->left, *root);
+			}
+			else
+			{
+				if (aux->top->data < aux->data)
+					aux->top->right = NULL;
+				else
+					aux->top->left = NULL;
+				insert_node(aux->left, aux->top);
+				insert_node(aux->right, aux->top);
+			}
+			delete aux;
+		}
 	};
 
 /*
