0eaf8982089d49f0f9aee64ec1754508bc9e64a6
Ingoleth <aiglesia97@gmail.com>
2022-06-17 16:15:43 +0200

    Fixy fixes!

--- ft_inception/srcs/wordpress/srcs/entrypoint.sh
+++ ft_inception/srcs/wordpress/srcs/entrypoint.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
-if [ ! -f /var/www/wordpress/index.php ]; then
+if [ ! -f /var/www/html/wordpress/index.php ]; then
 
 	tar xf latest.tar.gz -C ../var/www/html
 	wp config create --skip-check --path="/var/www/html/wordpress" --dbname=$WP_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost="mariadb" --allow-root
 	for i in {1..10}
 	do
-		echo patata $DOMAIN_NAME
-		if ! wp core install --path="/var/www/html/wordpress" --url="https://$DOMAIN_NAME" --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root ; then
+		if wp core install --path="/var/www/html/wordpress" --url=https://$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root ; then
+			echo Successfully installed wordpress!
 			break 1
 		fi
 		sleep 3;
 	done
+	echo "Im here"
 	wp user create $WP_USER $WP_USER_EMAIL --path="/var/www/html/wordpress" --role=author --user_pass=$WP_USER_PASSWORD --allow-root
-	echo "Successfully installed wordpress requirements!"
+	echo "Created wp user"
 fi
 
 echo "PHP FPM started on port 9000"
